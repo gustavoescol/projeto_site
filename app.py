6e179a182155d9caa568871dfce48c364bfc70dd
larissa-kochinski <larissa.kochinskicarvalho@escola.pr.gov.br>
2025-06-12 08:39:46 -0300

    m

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from routes.main_routes import main_bp
 from routes.usuario_routes import usuario_bp
-from routes.carrinho_routes import carrinho_bp
+
 
 app = Flask(__name__)
 
 app.register_blueprint(main_bp)
 app.register_blueprint(usuario_bp)
-app.register_blueprint(carrinho_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
